a9460f8795115189e4398e69e2e026526cdcb6db
Yulong Tan <swifties@uw.edu>
2015-07-19 12:55:14 -0700

    added ender trick method

--- app/src/main/java/yulongproductions/com/skatedice/TrickManager.java
+++ app/src/main/java/yulongproductions/com/skatedice/TrickManager.java
@@ -5,17 +5,26 @@ import java.util.List;
 
 /**
  * Created by Yulong on 7/11/2015.
+ *
+ * This class represents Dice that deal with Skateboarding.
+ * This class was intended to help me learn some more tricks,
+ * and it revolves around a dice that generates the stance
+ * (regular, switch, nollie, fakie), direction (frontside, backside),
+ * degrees (180, 360, 540), and then the trick itself.
  */
 public class TrickManager {
+    //Fields
     private List<String> tricks;
 
+    // Class constants
     public static final int STANCES = 4;
     public static final int DIRECTIONS = 2;
-    public static final int ROTATIONS = 4;
+    public static final int ROTATIONS = 3;
+    public static final boolean EASY = true;
 
     // Constructs a list of tricks from an external file
     public TrickManager() {
-        this.tricks = new ArrayList<String>();
+        this.tricks = new ArrayList<>();
         String[] trickList = {
                 "ollie", "nollie", "kickflip", "shuvit", "heelflip", "bigspin", "revert"
         };
@@ -53,10 +62,8 @@ public class TrickManager {
         Dice degrees = new Dice(ROTATIONS);
         int number = degrees.getNumber();
         if (number == 0) {
-            return 0;
-        } else if (number == 1) {
             return 180;
-        } else if (number == 2) {
+        } else if (number == 1) {
             return 360;
         } else {
             return 540;
@@ -66,13 +73,19 @@ public class TrickManager {
     // Returns the String representation of the trick name
     public String getTrick() {
         Dice trick = new Dice(this.tricks.size());
-        return " " + this.tricks.get(trick.getNumber());
+        return this.tricks.get(trick.getNumber());
     }
 
     // Returns the String representation of the full trick to do
     public String printTrick() {
-        return this.getStance() + this.getDirection()
-                + this.getDegrees() + this.getTrick();
+        String trick = this.getTrick();
+        if (EASY) {
+            return this.getStance() + this.getDirection()
+                    + this.getDegrees() + " " + trick;
+        } else { // not easy
+            return this.getStance() + this.getDirection()
+                    + this.getDegrees() + " " + trick + " " + this.ender(trick);
+        }
     }
 
     // Private method that tranfers all the elements of an array to an ArrayList of
@@ -80,6 +93,19 @@ public class TrickManager {
     private void transfer(String[] list, List<String> newList) {
         for (String item : list) {
             newList.add(item);
+        }
+    }
+
+    private String ender(String trick) {
+        String[] flipTricks = {"heelflip", "kickflip"};
+        Dice flipDice = new Dice(flipTricks.length);
+        if (trick.equals("heelflip") || trick.equals("kickflip")) {
+            return "shuvit";
+        } else if (trick.equals("shuvit")) {
+            // return either heelflip or kickflip
+            return flipTricks[flipDice.getNumber()];
+        } else {
+            return "";
         }
     }
 }
